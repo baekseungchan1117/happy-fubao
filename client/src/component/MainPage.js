74287f90e6738ac2b38dc0197294d06ed2b434e7
bhh9860 <bhh9860@naver.com>
2023-07-13 10:01:53 +0900

    m

--- client/src/component/MainPage.js
+++ client/src/component/MainPage.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
-import { Element, scroller } from 'react-scroll';
-import { useLocation } from 'react-router-dom';
-import About from '../component/Main/About'
-import Home from '../component/Main/Home'
-import Map from '../component/Main/Map'
-import Main from '../component/Main/Main'
-
+import React, { useEffect } from "react";
+import { Element, scroller } from "react-scroll";
+import { useLocation } from "react-router-dom";
+import About from "../component/Main/About";
+import Home from "../component/Main/Home";
+// import Map from '../component/Main/Map'
+import Main from "../component/Main/Main";
 
 const MainPage = () => {
   const location = useLocation();
@@ -15,25 +14,25 @@ const MainPage = () => {
       scroller.scrollTo(location.state.scrollTo, {
         duration: 1500,
         delay: 0,
-        smooth: 'easeInOutQuart'
+        smooth: "easeInOutQuart",
       });
     }
   }, [location]);
 
   return (
     <>
-      <Element name="main" >
+      <Element name="main">
         <Main />
       </Element>
-      <Element name="introduce" >
+      <Element name="introduce">
         <About />
       </Element>
       <Element name="today_ranking">
-        <Home/>
+        <Home />
       </Element>
-      <Element name="location_map">
+      {/* <Element name="location_map">
         <Map />
-      </Element>
+      </Element> */}
     </>
   );
 };
